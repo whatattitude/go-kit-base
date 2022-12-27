5170475f237ac027ac3d1d29ef84793278cb6565
b_nineKit <b_nineKit@qq.com>
2022-12-27 22:20:54 +0800

    add fp

---
+++ httpserver/library/fp/fp.go
@@ -0,0 +1,25 @@
+package fp
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func Fp(a ...any) string {
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		fmt.Printf("\nfile: %s, line: %d ", file, line)
+		for i := range a {
+			fmt.Print(a[i], "             ")
+
+		}
+
+		return fmt.Sprintf("\nfile: %s, line: %d ", file, line)
+	}
+	for i := range a {
+		fmt.Print(a[i], "             ")
+
+	}
+
+	return ""
+}
